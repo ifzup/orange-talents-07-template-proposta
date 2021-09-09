559fe66347bef37106661b156b201270646bbcb9
ifzup <iago.faria@zup.com.br>
2021-09-09 13:24:13 -0300

    Prometheus

--- src/main/java/br/com/zupacademy/ifzup/proposta/security/Seguranca.java
+++ src/main/java/br/com/zupacademy/ifzup/proposta/security/Seguranca.java
@@ -15,10 +15,14 @@ public class Seguranca extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http)throws Exception{
         http.authorizeRequests(authorizeRequests ->
             authorizeRequests
+                    .antMatchers(HttpMethod.POST, "/actuator/**").hasAuthority("SCOPE_escopo-proposta")
+                    .antMatchers(HttpMethod.GET, "/actuator/**").hasAuthority("SCOPE_escopo-proposta")
                     .antMatchers(HttpMethod.GET, "/propostas/**").hasAuthority("SCOPE_escopo-proposta")
                     .antMatchers(HttpMethod.POST, "/propostas/*").hasAuthority("SCOPE_escopo-proposta")
                     .antMatchers(HttpMethod.POST, "/cartoes/**").hasAuthority("SCOPE_escopo-proposta")
                     .antMatchers(HttpMethod.POST, "/bloqueio/**").hasAuthority("SCOPE_escopo-proposta")
+                    .antMatchers(HttpMethod.GET, "/actuator/prometheus").permitAll()
+                    .antMatchers(HttpMethod.GET, "/actuator/health").permitAll()
                     .anyRequest().authenticated())
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
